44892f19e7b244e9bc4505cbf648d0958e783cea
Guillermo Ceballos <willeeh@gmail.com>
2012-12-27 12:48:13 +0000

    Trying to fix a minor issue

--- src/main/java/com/appreferendum/helloworld/resources/HelloWorldResource.java
+++ src/main/java/com/appreferendum/helloworld/resources/HelloWorldResource.java
@@ -1,21 +1,25 @@
 package com.appreferendum.helloworld.resources;
 
 import com.appreferendum.helloworld.model.Statement;
-import com.google.common.base.Optional;
+
 import com.mongodb.DB;
 import com.yammer.metrics.annotation.Timed;
 import net.vz.mongodb.jackson.DBCursor;
 import net.vz.mongodb.jackson.JacksonDBCollection;
-import net.vz.mongodb.jackson.WriteResult;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @Path("/hello-world/id/{id}")
 @Produces(MediaType.APPLICATION_JSON)
 public class HelloWorldResource
 {
+    private static final Logger logger = LoggerFactory.getLogger(HelloWorldResource.class);
+
     private final DB db;
 
     public HelloWorldResource(DB db)
@@ -27,15 +31,19 @@ public class HelloWorldResource
     @Timed
     public Statement getHello(@PathParam("id") String id)
     {
+        logger.info("Getting statement");
+
         JacksonDBCollection<Statement, String> statements =
                         JacksonDBCollection.wrap(db.getCollection("statements"), Statement.class, String.class);
-        DBCursor<Statement> cursor = statements.find().is("id", id);
+        DBCursor<Statement> cursor = statements.find().is("_id", id);
 
         if (!cursor.hasNext())
         {
+            logger.info("Statement not found");
             throw new WebApplicationException(Response.Status.NOT_FOUND);
         }
 
+        logger.info("Statement found");
         return cursor.next();
     }
 }
