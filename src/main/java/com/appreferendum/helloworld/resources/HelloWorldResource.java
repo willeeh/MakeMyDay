786dde2047b1699cc600694ab37b7c95911bde41
Guillermo Ceballos <willeeh@gmail.com>
2012-12-27 12:48:13 +0000

    Some logs to know what's going on

--- src/main/java/com/appreferendum/helloworld/resources/HelloWorldResource.java
+++ src/main/java/com/appreferendum/helloworld/resources/HelloWorldResource.java
@@ -1,21 +1,25 @@
 package com.appreferendum.helloworld.resources;
 
 import com.appreferendum.helloworld.model.Statement;
-import com.google.common.base.Optional;
+
 import com.mongodb.DB;
 import com.yammer.metrics.annotation.Timed;
 import net.vz.mongodb.jackson.DBCursor;
 import net.vz.mongodb.jackson.JacksonDBCollection;
-import net.vz.mongodb.jackson.WriteResult;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @Path("/hello-world/id/{id}")
 @Produces(MediaType.APPLICATION_JSON)
 public class HelloWorldResource
 {
+    private static final Logger logger = LoggerFactory.getLogger(HelloWorldResource.class);
+
     private final DB db;
 
     public HelloWorldResource(DB db)
@@ -27,6 +31,8 @@ public class HelloWorldResource
     @Timed
     public Statement getHello(@PathParam("id") String id)
     {
+        logger.info("--> Entra en el método al menos!!!");
+
         JacksonDBCollection<Statement, String> statements =
                         JacksonDBCollection.wrap(db.getCollection("statements"), Statement.class, String.class);
         DBCursor<Statement> cursor = statements.find().is("id", id);
