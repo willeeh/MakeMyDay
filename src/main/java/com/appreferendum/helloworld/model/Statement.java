4bbd6cdc2d31a4ad427ef60b5e0e025fbfa41f09
Guillermo Ceballos <willeeh@gmail.com>
2012-12-27 11:38:48 +0000

    Trying to have a right example

--- src/main/java/com/appreferendum/helloworld/model/Statement.java
+++ src/main/java/com/appreferendum/helloworld/model/Statement.java
@@ -1,13 +1,31 @@
 package com.appreferendum.helloworld.model;
 
+import net.vz.mongodb.jackson.ObjectId;
+import javax.persistence.Id;
 
 public class Statement
 {
+    private String id;
+
     private String content;
 
     public Statement(String content)
     {
         this.content = content;
+    }
+
+    @Id
+    @ObjectId
+    public String getId()
+    {
+        return id;
+    }
+
+    @Id
+    @ObjectId
+    public void setId(String id)
+    {
+        this.id = id;
     }
 
     public String getContent()
