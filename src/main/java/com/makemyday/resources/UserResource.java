ceb8a532c468eeb3de0f7c1c12e860aabe5a15d4
Ferran Maylinch <ferranmaylinch@gmail.com>
2013-01-19 20:20:25 +0100

    PostResource

--- src/main/java/com/makemyday/resources/UserResource.java
+++ src/main/java/com/makemyday/resources/UserResource.java
@@ -16,42 +16,40 @@ import javax.ws.rs.core.UriBuilder;
 @Produces(MediaType.APPLICATION_JSON)
 public class UserResource
 {
-    private static final Logger logger = LoggerFactory.getLogger(UserResource.class);
+	private static final Logger logger = LoggerFactory.getLogger(UserResource.class);
 
-    private UserService userService;
+	private UserService userService;
 
-    @Inject
-    public UserResource(UserService userService)
-    {
-        this.userService = userService;
-    }
+	@Inject
+	public UserResource(UserService userService)
+	{
+		this.userService = userService;
+	}
 
-    @POST
-    @Consumes(MediaType.APPLICATION_JSON)
-    public Response signUp(@Valid User user)
-    {
-        if ( userService.createUser(user) )
-        {
-            return Response.created( UriBuilder.fromResource(UserResource.class).build(user.getFacebookId()) ).build();
-        }
-        else
-        {
-            return Response.status(Response.Status.NOT_MODIFIED).build();
-        }
-    }
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response signUp(@Valid User user)
+	{
+		if (userService.createUser(user))
+		{
+			return Response.created(UriBuilder.fromResource(UserResource.class).build(user.getFacebookId())).build();
+		} else
+		{
+			return Response.status(Response.Status.NOT_MODIFIED).build();
+		}
+	}
 
-    @GET
-    @Path("/{id}")
-    public User getUser(@PathParam("id") String facebookId)
-    {
-        User user = userService.getUser(facebookId);
+	@GET
+	@Path("/{id}")
+	public User getUser(@PathParam("id") String facebookId)
+	{
+		User user = userService.getUser(facebookId);
 
-        if (user == null)
-        {
-            throw new WebApplicationException(Response.Status.NOT_FOUND);
-        }
+		if (user == null)
+		{
+			throw new WebApplicationException(Response.Status.NOT_FOUND);
+		}
 
-        return user;
-
-    }
+		return user;
+	}
 }
