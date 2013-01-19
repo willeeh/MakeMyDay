96f7623012f24aa70d1b4858575d376e1306a944
Guillermo Ceballos <willeeh@gmail.com>
2013-01-19 18:11:14 +0100

    Entities created.

---
+++ src/main/java/com/makemyday/entities/Device.java
@@ -0,0 +1,45 @@
+package com.makemyday.entities;
+
+import com.google.code.morphia.annotations.Entity;
+import com.google.code.morphia.annotations.Id;
+import com.makemyday.entities.base.Identity;
+import org.bson.types.ObjectId;
+import org.hibernate.validator.constraints.NotEmpty;
+
+@Entity(value="devices", noClassnameStored=true)
+public class Device extends Identity
+{
+    @NotEmpty
+    private String udid;
+
+    private String token;
+
+    public Device()
+    {
+    }
+
+    public Device(String udid)
+    {
+        this.udid = udid;
+    }
+
+    public String getUdid()
+    {
+        return udid;
+    }
+
+    public void setUdid(String udid)
+    {
+        this.udid = udid;
+    }
+
+    public String getToken()
+    {
+        return token;
+    }
+
+    public void setToken(String token)
+    {
+        this.token = token;
+    }
+}
--- src/main/java/com/makemyday/entities/User.java
+++ src/main/java/com/makemyday/entities/User.java
@@ -2,41 +2,44 @@ package com.makemyday.entities;
 
 import com.google.code.morphia.annotations.Entity;
 import com.google.code.morphia.annotations.Id;
+import com.google.code.morphia.annotations.Reference;
+import com.makemyday.entities.base.Identity;
 import org.bson.types.ObjectId;
 import org.hibernate.validator.constraints.NotEmpty;
 
-@Entity(value="users", noClassnameStored=true)
-public class User
-{
-    private @Id
-    ObjectId id;
+import java.util.List;
 
-    private @NotEmpty
-    String facebookId;
-    private @NotEmpty String name;
-    private @NotEmpty String token;
+@Entity(value="users", noClassnameStored=true)
+public class User extends Identity
+{
+    @NotEmpty
+    private String facebookId;
+
+    private String accessToken;
+
+    private String name;
+
+    private String gender;
+
     private String email;
+
+    private String picture;
+
+    @Reference(lazy = true)
+    private List<Device> devices;
 
     public User()
     {
     }
 
-    public User(String facebookId, String name, String email, String token)
+    public User(String facebookId, String accessToken, String name, String gender, String email, String picture)
     {
         this.facebookId = facebookId;
+        this.accessToken = accessToken;
         this.name = name;
+        this.gender = gender;
         this.email = email;
-        this.token = token;
-    }
-
-    public ObjectId getId()
-    {
-        return id;
-    }
-
-    public void setId(ObjectId id)
-    {
-        this.id = id;
+        this.picture = picture;
     }
 
     public String getFacebookId()
@@ -49,6 +52,16 @@ public class User
         this.facebookId = facebookId;
     }
 
+    public String getAccessToken()
+    {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken)
+    {
+        this.accessToken = accessToken;
+    }
+
     public String getName()
     {
         return name;
@@ -57,6 +70,16 @@ public class User
     public void setName(String name)
     {
         this.name = name;
+    }
+
+    public String getGender()
+    {
+        return gender;
+    }
+
+    public void setGender(String gender)
+    {
+        this.gender = gender;
     }
 
     public String getEmail()
@@ -69,14 +92,13 @@ public class User
         this.email = email;
     }
 
-    public String getToken()
+    public String getPicture()
     {
-        return token;
+        return picture;
     }
 
-    public void setToken(String token)
+    public void setPicture(String picture)
     {
-        this.token = token;
+        this.picture = picture;
     }
-
 }
